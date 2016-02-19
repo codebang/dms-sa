3130fad29af2991dce203a327250d313e453ad2b
haoyan <neuyanhao@163.com>
2016-02-19 04:59:13 +0000

    change type to vmtype for logs

--- dms_sa_controller/lib/files/vrouter/post_steps.sh
+++ dms_sa_controller/lib/files/vrouter/post_steps.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 echo "post actions"
-sudo journalctl -f | log-courier -config /etc/log-courier/log-courier.conf -stdin &
-
+sudo  journalctl -f | log-courier -config /etc/log-courier/log-courier.conf -stdin 1>/dev/null 2>&1 &

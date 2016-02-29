5305a38efe39f2cd7df8cb53e4a2758690cbfaca
huiyu <huiyu@dadynet.com>
2016-02-29 16:57:22 +0800

    add inventory full sync to redis

--- dms_sa_controller/lib/dsoSynczoo/sync_redis.py
+++ dms_sa_controller/lib/dsoSynczoo/sync_redis.py
@@ -1,26 +1,46 @@
-from ..utils import logger
-from sync_kafka_client import InventoryKafkaClient as client
+# from ..utils import logger
+import json
 
 
 def sync_to_redis(account_info, vpn_clients):
-    logger.info("start sync dso info to redis, " + account_info.id)
+
+    result = []
+
+    # logger.info("start sync dso info to redis, " + account_info.id)
     account_id = account_info.id
 
     # account
-    client.send_create(
-        account_id, "account", account_info.to_kafka)
+    result.append(gen_create_msg(
+        account_id, "account", account_info.to_kafka()))
 
     # sync each group
     for group in account_info.groups:
-        client.send_create(account_id, "usergroup", group.to_kafka)
+        result.append(gen_create_msg(account_id, "usergroup", group.to_kafka()))
         for user in group.users:
-            client.send_create(account_id, "user", user.to_kafka(group))
+            result.append(gen_create_msg(account_id, "user", user.to_kafka(group)))
             for host in user.hosts:
-                client.send_create(account_id, "host", host.to_kafka_user_host(user))
+                result.append(gen_create_msg(account_id, "host", host.to_kafka_user_host(user)))
 
         for server in group.servers:
-            client.send_create(account_id, "host", server.to_kafka_group_host(group))
+            result.append(gen_create_msg(account_id, "host", server.to_kafka_group_host(group)))
 
     # sync vpn
     for vpn in vpn_clients:
-        client.send_create(account_id, "vpn", vpn.to_kafka)
+        result.append(gen_create_msg(account_id, "vpn", vpn.to_kafka()))
+
+    return result
+
+
+def inventory_json(account_id, module, operation, result, data):
+        message = {
+            "accountId": account_id,
+            "module": module,
+            "operation": operation,
+            "result": result,
+            "data": data
+        }
+        return json.dumps(message)
+
+
+def gen_create_msg(account_id, module, data):
+    return inventory_json(account_id, module, "create", "success", data)
